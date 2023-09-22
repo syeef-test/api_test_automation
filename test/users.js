8b1302330e85b3327c9db08c883c9d6ebbb0a2ac
syeef <syeefislam@yahoo.com>
2023-09-22 20:45:25 +0530

    first done succesfully change on package.json in test to use es6

---
+++ test/users.js
@@ -0,0 +1,15 @@
+import supertest from "supertest";
+
+const request = supertest("https://gorest.co.in/public-api/");
+
+const TOKEN =
+  "0d5486f43754e91ddf75b12af1dc17323b748b5066be95ce1befafed8c8946b6";
+
+describe("users", () => {
+  it("GET /users", () => {
+    request.get(`/users?access-token=${TOKEN}`).end((err, res) => {
+      console.log(res.body);
+      console.log(err);
+    });
+  });
+});
