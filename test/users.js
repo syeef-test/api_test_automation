87df3fac9b9906615d6452476b03f98cd783142c
syeef <syeefislam@yahoo.com>
2023-09-23 06:38:48 +0530

    first api test done

--- test/users.js
+++ test/users.js
@@ -1,15 +1,17 @@
 import supertest from "supertest";
 
+import { expect } from "chai";
+
 const request = supertest("https://gorest.co.in/public-api/");
 
 const TOKEN =
   "0d5486f43754e91ddf75b12af1dc17323b748b5066be95ce1befafed8c8946b6";
 
 describe("users", () => {
-  it("GET /users", () => {
+  it("GET /users", (done) => {
     request.get(`/users?access-token=${TOKEN}`).end((err, res) => {
-      console.log(res.body);
-      console.log(err);
+      expect(res.body.data).to.not.be.empty;
+      done();
     });
   });
 });
